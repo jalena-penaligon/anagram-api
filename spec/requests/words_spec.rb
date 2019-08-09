962daab280d90fdb8d11245a3cbb0addf00c57c6
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-08-08 21:44:22 -0600

    Adds test that a word not in the dictionary will not be added

--- spec/requests/words_spec.rb
+++ spec/requests/words_spec.rb
@@ -17,11 +17,19 @@ describe "Words API" do
     dear = DictionaryWord.create!(word: "dear")
     dare = DictionaryWord.create!(word: "dare")
     read = DictionaryWord.create!(word: "read")
-    
+
     body = {"words": ["read", "dear", "dare"] }
     post "/words", params: body
     expect(response).to have_http_status(201)
 
     expect(Word.count).to eq(3)
+  end
+
+  it 'will not add a word that does not exist in the dictionary' do
+    body = {"words": ["zyxwv"] }
+    post "/words", params: body
+    expect(response).to have_http_status(201)
+
+    expect(Word.count).to eq(0)
   end
 end
