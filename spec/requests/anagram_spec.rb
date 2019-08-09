db5dbc54a79b94c231660da6503ce5bc5d444925
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-08-08 21:44:08 -0600

    Adds test that multiple posts can be made and all anagrams will track

--- spec/requests/anagram_spec.rb
+++ spec/requests/anagram_spec.rb
@@ -5,6 +5,11 @@ describe "Anagrams API" do
     dear = DictionaryWord.create!(word: "dear")
     dare = DictionaryWord.create!(word: "dare")
     read = DictionaryWord.create!(word: "read")
+    read = DictionaryWord.create!(word: "adder")
+    read = DictionaryWord.create!(word: "adred")
+    read = DictionaryWord.create!(word: "dread")
+    read = DictionaryWord.create!(word: "dared")
+    read = DictionaryWord.create!(word: "readd")
   end
   it 'can get anagrams of a word' do
     dear = Word.create!(name: "dear")
@@ -43,5 +48,30 @@ describe "Anagrams API" do
     words = JSON.parse(response.body)
 
     expect(words["anagrams"].count).to eq(1)
+  end
+
+  it 'can add words to the corpus in multiple rounds and all anagrams will be tracked' do
+    body = {"words": ["adder", "adred", "dread"] }
+    post "/words", params: body
+
+    get "/anagrams/dread"
+    words = JSON.parse(response.body)
+
+    expect(words["anagrams"].count).to eq(2)
+
+    body = {"words": ["dared", "readd"] }
+    post "/words", params: body
+
+    get "/anagrams/dread"
+    words = JSON.parse(response.body)
+
+    expect(words["anagrams"].count).to eq(4)
+  end
+
+  it 'will return an empty array for a word that has no anagrams' do
+    get "/anagrams/zyxwv"
+    words = JSON.parse(response.body)
+
+    expect(words["anagrams"].count).to eq(0)
   end
 end
