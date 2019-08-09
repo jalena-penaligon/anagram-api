f20fdc9a2e36d7aec0251826c6c880d2ba397a94
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-08-08 20:51:39 -0600

    Adds test that anagrams can be populated immediate after posting new words

--- spec/requests/anagram_spec.rb
+++ spec/requests/anagram_spec.rb
@@ -5,11 +5,33 @@ describe "Anagrams API" do
     dear = Word.create!(name: "dear")
     dare = Word.create!(name: "dare")
     read = Word.create!(name: "read")
+    aw_1 = AnagramWord.create(word_id: read.id, anagram_id: dear.id, anagram_name: "dear")
+    aw_2 = AnagramWord.create(word_id: read.id, anagram_id: dare.id, anagram_name: "dare")
 
     get "/anagrams/read"
     expect(response).to be_successful
-    binding.pry
+
     words = JSON.parse(response.body)
-    expect(words.count).to eq(3)
+
+    expect(words["anagrams"].count).to eq(2)
+    expect(words["anagrams"][0]).to eq("dare")
+    expect(words["anagrams"][1]).to eq("dear")
+  end
+
+  it 'can add words to the corpus, then view their anagrams' do
+    dear = DictionaryWord.create!(word: "dear")
+    dare = DictionaryWord.create!(word: "dare")
+    read = DictionaryWord.create!(word: "read")
+
+    body = {"words": ["read", "dear", "dare"] }
+    post "/words", params: body
+
+    get "/anagrams/dare"
+    words = JSON.parse(response.body)
+    binding.pry
+
+    expect(words["anagrams"].count).to eq(2)
+    expect(words["anagrams"][0]).to eq("dear")
+    expect(words["anagrams"][1]).to eq("read")
   end
 end
