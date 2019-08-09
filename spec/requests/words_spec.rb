bffc6ed760ead6cb16e85972f5824923a319faa2
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-08-08 22:16:47 -0600

    Adds tests that all or single words can be deleted

--- spec/requests/words_spec.rb
+++ spec/requests/words_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe "Words API" do
+  before(:each) do
+    dear = DictionaryWord.create!(word: "dear")
+    dare = DictionaryWord.create!(word: "dare")
+    read = DictionaryWord.create!(word: "read")
+  end
+
   it 'can get words from the corpus' do
     dear = Word.create!(name: "dear")
     dare = Word.create!(name: "dare")
@@ -14,10 +20,6 @@ describe "Words API" do
   end
 
   it 'can add words to the corpus' do
-    dear = DictionaryWord.create!(word: "dear")
-    dare = DictionaryWord.create!(word: "dare")
-    read = DictionaryWord.create!(word: "read")
-
     body = {"words": ["read", "dear", "dare"] }
     post "/words", params: body
     expect(response).to have_http_status(201)
@@ -31,5 +33,38 @@ describe "Words API" do
     expect(response).to have_http_status(201)
 
     expect(Word.count).to eq(0)
+  end
+
+  it 'can delete all words from the corpus' do
+    body = {"words": ["read", "dear", "dare"] }
+    post "/words", params: body
+    expect(Word.count).to eq(3)
+
+    delete "/words"
+    expect(response).to have_http_status(204)
+    expect(Word.count).to eq(0)
+  end
+
+  it 'can delete a single word from the corpus' do
+    body = {"words": ["read", "dear", "dare"] }
+    post "/words", params: body
+    expect(Word.count).to eq(3)
+
+    delete "/words/dear"
+    expect(response).to have_http_status(204)
+    expect(Word.count).to eq(2)
+  end
+
+  it 'deleting a single word also deletes associated anagrams' do
+    body = {"words": ["read", "dear", "dare"] }
+    post "/words", params: body
+    expect(Word.count).to eq(3)
+
+    delete "/words/dear"
+    get "/anagrams/read"
+    anagrams = JSON.parse(response.body)["anagrams"]
+
+    expect(anagrams.count).to eq(1)
+    expect(anagrams[0]).to eq("dare")
   end
 end
