2d80dfbb211ef11a1322c2f6ab0cebc27c2e21b0
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-08-09 02:29:51 -0600

    Refactors without anagram_word references

--- app/models/word.rb
+++ app/models/word.rb
@@ -4,36 +4,16 @@ class Word < ApplicationRecord
   def self.bulk_create(words)
     words.each do |word|
       if DictionaryWord.find_by(word: word) != nil
-        new_word = Word.create(name: word, char_count: word.length)
-        new_word.find_anagrams
+        key = word.split("").sort.join
+        new_word = Word.create(name: word, key: key)
       end
     end
   end
 
-  def find_anagrams
-    potential_anagrams = Word.where(char_count: self.name.length)
-    potential_anagrams.each do |anagram|
-      if self.valid_anagram(anagram)
-        add_anagrams(anagram, self)
-      end
-    end
-  end
-
-  def valid_anagram(anagram)
-    anagram.id != self.id && arrange(anagram.name) == arrange(self.name)
-  end
-
-  def arrange(word)
-    word.split("").sort
-  end
-
-  def add_anagrams(anagram, word)
-    create_anagram_word(anagram, self) && create_anagram_word(self, anagram)
-  end
-
-  def create_anagram_word(word, anagram)
-    AnagramWord.find_or_create_by(anagram_id: anagram.id,
-                                  word_id: word.id,
-                                  anagram_name: anagram.name)
+  def self.anagrams(word, limit)
+    where(key: word.key)
+    .where.not(id: word.id)
+    .limit(limit)
+    .pluck(:name).sort
   end
 end
