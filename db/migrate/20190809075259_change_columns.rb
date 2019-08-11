13f288434d1511acd8287b1a931a4b0f856cba62
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-08-11 15:35:27 -0600

    Readds char_count to schema to simplify queries

--- db/migrate/20190809075259_change_columns.rb
+++ db/migrate/20190809075259_change_columns.rb
@@ -2,7 +2,6 @@ class ChangeColumns < ActiveRecord::Migration[5.2]
   def change
     add_column :dictionary_words, :key, :string
 
-    rename_column :words, :char_count, :key
-    change_column :words, :key, :string
+    add_column :words, :key, :string
   end
 end
