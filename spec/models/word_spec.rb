90268c5f32f0db9f962ad8af46073f60c187591a
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-08-11 21:29:32 -0600

    Adds additional testing and refactors for readability

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -23,14 +23,6 @@ RSpec.describe Word, type: :model do
       expect(Word.count).to eq(3)
     end
 
-    it 'anagrams finds all anagrams from corpus' do
-      dear = Word.create(name: "dear", key: "ader", char_count: 4)
-      dare = Word.create(name: "dare", key: "ader", char_count: 4)
-      read = Word.create(name: "read", key: "ader", char_count: 4)
-
-      expect(Word.anagrams(dear)).to eq(["dare", "read"])
-    end
-
     it 'statistics returns a hash of stat info' do
       dear = Word.create!(name: "dear", key: "ader", char_count: 4)
       abstract = Word.create!(name: "abstract", key: "aabcrstt", char_count: 8)
@@ -40,6 +32,34 @@ RSpec.describe Word, type: :model do
       expect(Word.statistics[:min_length]).to eq(4)
       expect(Word.statistics[:max_length]).to eq(8)
       expect(Word.statistics[:average_length]).to eq(5.67)
+    end
+    describe 'anagram methods' do
+      before(:each) do
+        @dear = Word.create(name: "dear", key: "ader", char_count: 4)
+        @dare = Word.create(name: "dare", key: "ader", char_count: 4)
+        @read = Word.create(name: "read", key: "ader", char_count: 4)
+        @drae = Word.create(name: "Drae", key: "ader", char_count: 4)
+      end
+
+      it 'anagrams finds all anagrams from corpus' do
+        expect(Word.anagrams(@dear)).to eq(["Drae", "dare", "read"])
+      end
+
+      it 'filtered_anagrams returns only words that are not proper nouns' do
+        expect(Word.anagrams(@dear, nil, "false")).to eq(["dare", "read"])
+      end
+
+      it 'most_anagrams returns an array of words that have the most anagrams' do
+        expect(Word.most_anagrams).to eq(["Drae", "dare", "dear", "read"])
+      end
+
+      it 'anagram_matcher returns true if words are anagrams' do
+        words = ["dear", "read", "dare"]
+        expect(Word.anagram_matcher(words)).to eq(true)
+
+        words = ["dread", "read"]
+        expect(Word.anagram_matcher(words)).to eq(false)
+      end
     end
   end
 end
