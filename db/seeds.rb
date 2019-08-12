bd857e34ba05ed10d3518cb1c2f0d022c45170d9
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-08-12 01:15:47 -0600

    Updates seedfile for AWS deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+words = DictionaryWord.all
+words.destroy_all
+
 words = File.open('db/dictionary.txt').map do |line|
   key = line.strip.split("").sort.join
   DictionaryWord.new(word: line.strip, key: key)
